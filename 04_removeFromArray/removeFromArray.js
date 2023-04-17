8d0864b2cdcca733bb0b75ac077a923d744a1ac4
Craig L <craig.leeson@utexas.edu>
2023-04-17 01:37:23 -0500

    finished ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...vals) {
+  const output = [];
 
+  array.forEach((element) => {
+    if (!vals.includes(element)) {
+      output.push(element);
+    }
+  });
+
+  return output;
 };
 
 // Do not edit below this line
