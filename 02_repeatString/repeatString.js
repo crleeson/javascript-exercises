34496f184e96bb15f03d2e6f458111b09f34dac6
Craig L <craig.leeson@utexas.edu>
2023-04-17 01:14:39 -0500

    Finished ex 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, numTimes) {
+  let output = "";
 
+  if (numTimes < 0) {
+    output = "ERROR";
+  } else {
+    for (let i = 0; i < numTimes; i++) {
+      output += string;
+    }
+  }
+
+  return output;
 };
 
 // Do not edit below this line
