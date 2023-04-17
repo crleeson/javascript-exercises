b1215d64cab14b77aa29ee5bc05f9b70a4cf6bf4
Craig L <craig.leeson@utexas.edu>
2023-04-17 01:41:14 -0500

    finished ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (starting, ending) {
+  if (
+    typeof starting !== "number" ||
+    typeof ending !== "number" ||
+    starting < 0 ||
+    ending < 0
+  ) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  for (
+    let i = Math.min(starting, ending);
+    i < Math.max(starting, ending) + 1;
+    i++
+  ) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
