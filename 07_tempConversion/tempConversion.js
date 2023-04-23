0ab4236930940a69282397798b80b08cf755378e
Craig L <craig.leeson@utexas.edu>
2023-04-23 02:52:55 -0500

    finished ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+let THIRTY_TWO = 32;
+
+const convertToCelsius = function (tempFarenheit) {
+  let CONVERSION_FACTOR = (1.0 * 5) / 9;
+
+  return Math.round((tempFarenheit - THIRTY_TWO) * CONVERSION_FACTOR * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempCelsius) {
+  let CONVERSION_FACTOR = (1.0 * 9) / 5;
+
+  return Math.round((tempCelsius * CONVERSION_FACTOR + THIRTY_TWO) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
